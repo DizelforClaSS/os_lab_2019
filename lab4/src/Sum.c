cde51a98526b9fc5478ed9915bbd6641be9f0b22
vlad <machomen200073@gmail.com>
2020-04-26 19:36:40 +0300

    Added Sum, Parallel programms and makefiles

---
+++ lab4/src/Sum.c
@@ -0,0 +1,17 @@
+#include "Sum.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int Sum(int* array,int begin,int end) {
+  int sum = 0;
+  // TODO: your code here 
+  
+  for(int i=begin; i<=end;i++)
+  {
+      
+	sum+=array[i];
+        
+  }
+  return sum;
+}
